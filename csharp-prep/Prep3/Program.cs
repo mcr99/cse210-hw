8aa2dba2b3d03c18b5912ddd1ddae85a0d081a1f
mcr99 <mcanastuj1999@gmail.com>
2024-01-23 00:01:50 -0600

    adding part number 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // int number = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next (1,100);
+
+        int guess = 0;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("YOU GUESSED IT! ");
+            }
+        } while (guess != number);
+
     }
 }
