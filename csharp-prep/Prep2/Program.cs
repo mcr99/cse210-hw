8d489b866d35c062b68badf0be3dd7858d3dd444
mcr99 <mcanastuj1999@gmail.com>
2024-01-22 23:36:22 -0600

    adding 2 part

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Diagnostics;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your Porcentage: ");
+        string Porcentage = Console.ReadLine();
+
+        int x = int.Parse(Porcentage);
+        string letter = "";
+
+        if (x >= 90)
+        {
+            letter = "A" ;
+        }
+
+        else if ( x >= 80 )
+        {
+            letter = "B";
+        }
+        
+        else if ( x >= 70 )
+        {
+            letter = "C";
+        }
+
+        else if ( x>= 60 )
+        {
+            letter = "D";
+        }
+
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if ( x >= 70 )
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You Will get it next time! ");
+        }
     }
 }
