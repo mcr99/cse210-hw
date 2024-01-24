8175737992af3928d2337dc49105409b064b5f46
mcr99 <mcanastuj1999@gmail.com>
2024-01-23 23:57:48 -0600

    addind part 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.ComponentModel;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int uNumber = 0;
+
+        do
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            string response = Console.ReadLine();
+            uNumber = int.Parse(response);
+            if (uNumber !=0 );
+            {
+                numbers.Add(uNumber);
+            }
+        } while (uNumber !=0);
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum} ");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The Average is: {average} ");
+
+        int max = numbers[0];
+        {
+            
+            foreach (int number in numbers)
+            if (number > max)
+            {
+                max = number;
+            }
+            Console.WriteLine($"The max is: {max}");
+
+        }
     }
 }
